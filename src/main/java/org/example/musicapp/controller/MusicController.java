4c4bc2cd0bef9a83656a7dd2aaed9bb8b3316443
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-04-19 00:35:32 +0100

    update initial commit

--- src/main/java/org/example/musicapp/controller/MusicController.java
+++ src/main/java/org/example/musicapp/controller/MusicController.java
@@ -3,75 +3,85 @@ package org.example.musicapp.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.musicapp.model.Music;
-import org.example.musicapp.model.MusicResource;
 import org.example.musicapp.service.MusicService;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("api/v1/music")
 @RequiredArgsConstructor
 public class MusicController {
     private final MusicService musicService;
-@GetMapping("allMusic")
-public ResponseEntity<Iterable<Music>> getAllMusic(){
-//public ResponseEntity<List<Music>> getAllMusic(){
-        return musicService.getAllMusic();
-        }
-@DeleteMapping("delete/{id}")
+
+
+    private final ThymeleafController thymeleafController;
+
+@DeleteMapping("/delete/{id}")
 public ResponseEntity<String>deleteMusic(@PathVariable int id){
     return musicService.deleteMusic(id);
         }
-        @PutMapping("update/{id}")
-    public ResponseEntity<Music> updateMusic(@PathVariable int id,@Valid @RequestBody Music music){
+
+        @PutMapping("/update/{id}")
+    public ResponseEntity<Music> updateMusic(@Valid @PathVariable int id, @RequestBody Music music){
     return musicService.updateMusic(id, music);
         }
+
     @GetMapping("getMusic/{id}")
     public ResponseEntity<Music> getMusicById(@PathVariable int id){
         return musicService.getMusicById(id);
     }
-//        @GetMapping("/{id}")
-//    public ResponseEntity<MusicResource>getMusicResource(@PathVariable int id){
-//    Music musicToSend = musicService.getMusicById(id).getBody();
-//            MusicResource musicResource = new MusicResource();
-//            musicResource.setMusic(musicToSend);
-//            Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MusicController.class).getMusicById(id)).withSelfRel();
-//            Link delete = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MusicController.class).deleteMusic(id)).withRel("delete");
-//            Link update = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MusicController.class).updateMusic(id,musicToSend)).withRel("update");
-//            musicResource.add(selfLink, delete,update);
-//            return new ResponseEntity<>(musicResource, HttpStatus.OK);
-//
-//        }
-        @PostMapping("SaveMusic")
+
+        @PostMapping("/SaveMusic")
     public ResponseEntity<Music> addNewMusic (@RequestBody @Valid Music music){
     return musicService.addNewMusic(music);
         }
+
         @GetMapping("artistName")
     public ResponseEntity<Music> findByArtistName(@RequestParam String artistName){
         return musicService.findByArtistName(artistName);
     }
+
     @GetMapping("title")
     public ResponseEntity<Music> findByTitle (@RequestParam String title){
         return musicService.findByTitle(title);
     }
+
     @GetMapping("genre")
     public ResponseEntity<Music>findByGenre(@RequestParam String genre){
         return musicService.findByGenre(genre);
     }
+
     @GetMapping("year")
     public ResponseEntity<Music>findByYearOfProduction(@RequestParam int year){
         return musicService.findByYearOfProduction(year);
     }
+
     @GetMapping("albumName")
     public ResponseEntity<Music>findByAlbumName(@RequestParam String albumName){
         return musicService.findByAlbumName(albumName);
     }
 
+    @GetMapping("allMusic")
+    public ResponseEntity<Iterable<Music>> getAllMusic(){
+        return musicService.getAllMusic();
+    }
+
+
+    @GetMapping("addMusic")
+    public ModelAndView registrationForm(){
+        ModelAndView musicForm = new ModelAndView("addMusic");
+        Music music = new Music();
+        musicForm.addObject("addMusic", music);
+        return musicForm;
+   }
+
+    @PostMapping("addMusic")
+    public ModelAndView addMusic(@ModelAttribute @Valid Music music){
+        musicService.addNewMusic(music);
+        return thymeleafController.displayMusic();
+
+    }
 }
 
--- src/main/java/org/example/musicapp/service/MusicService.java
+++ src/main/java/org/example/musicapp/service/MusicService.java
@@ -5,7 +5,6 @@ import org.example.musicapp.model.Music;
 import org.example.musicapp.repository.MusicRepository;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,23 +15,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MusicService {
     private final MusicRepository musicRepository;
-//    public ResponseEntity<List<Music>> getAllMusic(){
-    @Cacheable(value = {"AllMusic", "getMusicById"}, key = "#id")
+//    @Cacheable(value = {"AllMusic", "getMusic"}, key = "#id")
     public ResponseEntity<Iterable<Music>> getAllMusic(){
         System.out.println("catchable");
         return new ResponseEntity<>(musicRepository.findAll(), HttpStatus.OK);
 
     }
-    @Cacheable(value = "getMusicById", key = "#id")
+//    @Cacheable(value = "getMusic", key = "#id")
     public ResponseEntity<Music> getMusicById(int id){
         return new ResponseEntity<>(musicRepository.findById(id).get(),HttpStatus.OK);
     }
 
-    @CacheEvict(value = "addNewMusic", allEntries = true)
+//    @CacheEvict(value = "allMusic", allEntries = true)
     public ResponseEntity<Music> addNewMusic(Music music){
         return new ResponseEntity<>(musicRepository.save(music), HttpStatus.CREATED);
     }
-    @CacheEvict(value = {"updateMusic", "getMusicById"}, key = "#id", allEntries = true)
+//    @CacheEvict(value = {"allMusic", "getMusic"}, key = "#id")
     public ResponseEntity<Music> updateMusic(int id, Music music){
         Music dbMusic = musicRepository.findById(id).get();
         dbMusic.setTitle(music.getTitle());
@@ -43,31 +41,31 @@ public class MusicService {
         dbMusic.setYearOfProduction(music.getYearOfProduction());
         return new ResponseEntity<>(musicRepository.save(dbMusic),HttpStatus.ACCEPTED);
     }
-    @CacheEvict(value = "deleteMusic",allEntries = true)
+//    @CacheEvict(value = {"allMusic","getMusic" },allEntries = true)
     public ResponseEntity<String> deleteMusic(int id){
         Music music = musicRepository.findById(id).get();
         musicRepository.deleteById(id);
-        return new ResponseEntity<>("music is deleted Successfully", HttpStatus.OK);
+        return new ResponseEntity<>(music + "music is deleted Successfully", HttpStatus.OK);
     }
 
-    @Cacheable(value = "findByArtistName", key = "#artistName")
+//    @Cacheable(value = "artistName", key = "#artistName")
     public ResponseEntity<Music> findByArtistName(String artistName){
         return new ResponseEntity<>(musicRepository.findByArtistName(artistName), HttpStatus.FOUND);
     }
-    @Cacheable(value = "findByTitle", key = "#title")
+//    @Cacheable(value = "title", key = "#title")
     public ResponseEntity<Music> findByTitle (String title){
         return new ResponseEntity<>(musicRepository.findByTitle(title),HttpStatus.OK);
     }
-    @Cacheable(value = "findByGenre", key = "#genre")
+//    @Cacheable(value = "genre", key = "#genre")
     public ResponseEntity<Music>findByGenre(String genre){
         return new ResponseEntity<>(musicRepository.findByGenre(genre),HttpStatus.OK);
     }
-    @Cacheable(value = "findByYearOfProduction", key = "#year")
+//    @Cacheable(value = "year", key = "#year")
     public ResponseEntity<Music>findByYearOfProduction(int year){
         return new ResponseEntity<>(musicRepository.findByYearOfProduction(year), HttpStatus.OK);
 
     }
-    @Cacheable(value = "findByAlbumName", key = "#albumName")
+//    @Cacheable(value = "albumName", key = "#albumName")
     public ResponseEntity<Music>findByAlbumName(String albumName){
         return new ResponseEntity<>(musicRepository.findByAlbumName(albumName), HttpStatus.OK);
     }
